ae9082510abe471b0a06bd309867a5fcb50185e0
wipzhu <wipzhu@qq.com>
2024-01-31 15:26:46 +0800

    数据持久化-用户首选项

--- HelloWorld/entry/src/main/ets/entryability/EntryAbility.ts
+++ HelloWorld/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,10 +1,13 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import type window from '@ohos.window';
+import PreferencesUtil from '../common/util/PreferencesUtil'
 
 export default class EntryAbility extends UIAbility {
-  onCreate(want, launchParam) {
+  async onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    // 加载Preferences
+    await PreferencesUtil.loadPreference(this.context, 'MyPreferences')
   }
 
   onDestroy() {
